29ce2b452209d3005786aa8d7c42838cd690a5df
FernandoCahuex <fcahuexg@miumg.edu.gt>
2025-09-16 18:57:54 -0600

    Implemetacion Capa controlador

---
+++ Calculadora/CapaControladorEmpleados/Capa_Controlador_Empleados.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+
+namespace CapaControladorEmpleados
+{
+    internal class Capa_Controlador_Empleados
+    {
+        // LLenar una tabla CAPA CONTROLADOR
+
+
+        public DataTable llenarTbl(string tabla)
+        {
+            OdbcDataAdapter dt = sn.llenarTbl(tabla);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+    }
+}
